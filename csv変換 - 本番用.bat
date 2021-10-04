f1f443e3d3a5b37dafb8503e909a9dd085eebdfc
Cacaonib2020 <91684527+Cacaonib2020@users.noreply.github.com>
2021-10-04 16:20:40 +0900

    Update csv変換 - 本番用.bat

--- csv変換 - 本番用.bat
+++ csv変換 - 本番用.bat
@@ -5,7 +5,7 @@ mysql -h 172.16.18.254 -u user1 -pM6peZu kanjosen -e "INSERT INTO status(ID) val
 
 :loop
 mysql -h 172.16.18.254 -u user1 -pM6peZu kanjosen -e "LOAD DATA INFILE '/var/lib/mysql/fortest/import/imp.csv' INTO TABLE temp FIELDS TERMINATED BY ','; " 
-mysql -h 172.16.18.254 -u user1 -pM6peZu kanjosen -e "SET @ID=0;INSERT INTO status (ID,time,emo,temper,heart) SELECT * from (SELECT @ID:=@ID+1 as ID,time,temper,heart FROM temp ORDER BY time) AS data WHERE ID > (SELECT MAX(ID) FROM status);"
+mysql -h 172.16.18.254 -u user1 -pM6peZu kanjosen -e "SET @ID=0;INSERT INTO status (ID,time,temper,heart) SELECT * from (SELECT @ID:=@ID+1 as ID,time,temper,heart FROM temp ORDER BY time) AS data WHERE ID > (SELECT MAX(ID) FROM status);"
 mysql -h 172.16.18.254 -u user1 -pM6peZu kanjosen -e "DELETE FROM temp;"
 goto :loop
 
